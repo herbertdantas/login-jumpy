7d078340e82789fbbf009a864d8f5fba08ce9dc7
herbertdantas <herbertbezerradasilva@gmail.com>
2023-01-11 22:03:25 -0300

    alterando validações do formulário

---
+++ script.js
@@ -0,0 +1,60 @@
+const button = document.getElementById("botao")
+
+button.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    const nome = document.getElementById("name").value
+    const email = document.getElementById("email").value
+
+    
+
+    if (nome == ""){
+
+        document.getElementById("campo-nome").style.display = "block"
+        
+        
+    } else{
+        document.getElementById("campo-nome").style.display = "none"
+    }
+
+
+    if (email == ""){
+
+        document.getElementById("campo-email").style.display = "block"
+    }else{
+        document.getElementById("campo-email").style.display = "none"
+    }
+
+    if (nome != "" && email != ""){
+        document.getElementById("campo-obg").innerHTML = "Sucesso"
+        document.getElementById("campo-obg").style.display = "block"
+        document.getElementById("campo-obg").style.color = "green"
+        limpar()
+        
+    
+    }else{
+
+        document.getElementById("campo-obg").style.display = "block"
+        document.getElementById("campo-obg").innerHTML = "Campos obrigatórios não registrados"
+        document.getElementById("campo-obg").style.color = "#E73550"
+        
+        
+        
+        
+    }
+    
+
+})
+
+function limpar(){
+    document.getElementById("name").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("tel").value = ""
+    document.getElementById("cpf").value = ""
+    document.getElementById("senha").value = ""
+    
+}
+
+function sair(){
+    document.getElementById("campo-obg").style.display = "none"
+}
